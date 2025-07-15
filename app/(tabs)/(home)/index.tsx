3a4c2c77216812eeababeea1576e840c4480935d
Jinhyuck Kim <gandolkim@gmail.com>
2025-07-15 21:54:46 +0900

    Lecutre::8.conditional tab navigation

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,19 +1,26 @@
-import { useRouter } from "expo-router";
+import { usePathname, useRouter } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+  const pathname = usePathname();
+
+  console.log({ pathname });
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <View>
         <TouchableOpacity onPress={() => router.push("/")}>
-          <Text>For You </Text>
+          <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+            For You{" "}
+          </Text>
         </TouchableOpacity>
       </View>
       <View>
         <TouchableOpacity onPress={() => router.push("/following")}>
-          <Text>Following</Text>
+          <Text style={{ color: pathname === "/" ? "black" : "red" }}>
+            Following
+          </Text>
         </TouchableOpacity>
       </View>
       <View>
