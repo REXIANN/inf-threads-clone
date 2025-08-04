ddde35ba3035eba40e1d6856ef7abbbedc5fec00
Jinhyuck Kim <gandolkim@gmail.com>
2025-08-04 18:58:08 +0900

    Lecture::33:react-native-reanimated

--- app/(tabs)/(home)/following.tsx
+++ app/(tabs)/(home)/following.tsx
@@ -1,40 +1,121 @@
 import Post, { type Post as PostType } from "@/components/Post";
 import { FlashList } from "@shopify/flash-list";
-import { useState } from "react";
-import { StyleSheet, useColorScheme, View } from "react-native";
+import * as Haptics from "expo-haptics";
+import { usePathname } from "expo-router";
+import { useCallback, useContext, useRef, useState } from "react";
+import { PanResponder, StyleSheet, useColorScheme, View } from "react-native";
+import Animated, {
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { AnimationContext } from "./_layout";
+
+const AnimatedFlashList = Animated.createAnimatedComponent(FlashList<PostType>);
 
 export default function Following() {
   const colorScheme = useColorScheme();
+  const path = usePathname();
 
   const [posts, setPosts] = useState<PostType[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const onEndReached = () => {
-    if (posts.length > 0) {
-      fetch(`/posts?type=following&cursor=${posts.at(-1)?.id}`)
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.posts.length > 0) {
-            setPosts((prev) => [...prev, ...data.posts]);
-          }
+  const scrollPosition = useSharedValue(0);
+  const isReadyToRefresh = useSharedValue(false);
+
+  const { pullDownPosition } = useContext(AnimationContext);
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollPosition.value = event.contentOffset.y;
+    },
+  });
+
+  const onEndReached = useCallback(() => {
+    console.log("onEndReached", posts.at(-1)?.id);
+    fetch(`/posts?type=following&cursor=${posts.at(-1)?.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.posts.length > 0) {
+          setPosts((prev) => [...prev, ...data.posts]);
+        }
+      });
+  }, [posts, path]);
+
+  const onRefresh = (done: () => void) => {
+    setPosts([]);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    fetch("/posts?type=following")
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data.posts);
+      })
+      .finally(() => {
+        done();
+      });
+  };
+
+  const onPanRelease = () => {
+    pullDownPosition.value = withTiming(isReadyToRefresh.value ? 60 : 0, {
+      duration: 180,
+    });
+
+    if (isReadyToRefresh.value) {
+      onRefresh(() => {
+        pullDownPosition.value = withTiming(0, {
+          duration: 180,
         });
+      });
     }
   };
 
+  const panResponderRef = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gestureState) => {
+        const max = 120;
+
+        pullDownPosition.value = Math.max(Math.min(gestureState.dy, max), 0);
+        if (pullDownPosition.value >= max / 2 && !isReadyToRefresh.value) {
+          isReadyToRefresh.value = true;
+        }
+
+        if (pullDownPosition.value < max / 2 && isReadyToRefresh.value) {
+          // 잡아당기다 돌려놓으면 리프레시 안함
+          isReadyToRefresh.value = false;
+        }
+      },
+      onPanResponderRelease: onPanRelease,
+      onPanResponderTerminate: onPanRelease,
+    })
+  );
+
+  const pullDownStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: pullDownPosition.value }],
+  }));
+
   return (
-    <View
+    <Animated.View
       style={[
         styles.container,
         colorScheme === "dark" ? styles.containerDark : styles.containerLight,
+        pullDownStyle,
       ]}
+      {...panResponderRef.current.panHandlers}
     >
-      <FlashList
+      <AnimatedFlashList
+        refreshControl={<View />}
+        refreshing={refreshing}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
         estimatedItemSize={350}
         data={posts}
         renderItem={({ item }) => <Post item={item} />}
         onEndReachedThreshold={2}
         onEndReached={onEndReached}
       />
-    </View>
+    </Animated.View>
   );
 }
 
